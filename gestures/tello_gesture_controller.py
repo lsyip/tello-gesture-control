37b5ffc9653bdf06b7c6d39fa13377faf49416c5
Lynn Yip <50850731+lsyip@users.noreply.github.com>
2024-04-19 19:42:08 -0400

    Add directional gesture controls

--- gestures/tello_gesture_controller.py
+++ gestures/tello_gesture_controller.py
@@ -7,65 +7,62 @@ class TelloGestureController:
         self._is_landing = False
         self._is_flying = False
 
-        # RC control velocities
-        self.forw_back_velocity = 0
-        self.up_down_velocity = 0
-        self.left_right_velocity = 0
-        self.yaw_velocity = 0
 
     def gesture_control(self, gesture_buffer):
         gesture_id = gesture_buffer.get_gesture()
         print("GESTURE", gesture_id)
 
         if not self._is_landing:
-            """
-            if gesture_id == 0:  # Forward
-                self.forw_back_velocity = 30
-            elif gesture_id == 1:  # STOP
-                self.forw_back_velocity = self.up_down_velocity = \
-                    self.left_right_velocity = self.yaw_velocity = 0
-            if gesture_id == 5:  # Back
-                self.forw_back_velocity = -30
 
-            elif gesture_id == 2:  # UP
-                self.up_down_velocity = 25
-            elif gesture_id == 4:  # DOWN
-                self.up_down_velocity = -25
-
-            elif gesture_id == 3:  # LAND
-                self._is_landing = True
-                self.forw_back_velocity = self.up_down_velocity = \
-                    self.left_right_velocity = self.yaw_velocity = 0
-                self.tello.land()
-
-            elif gesture_id == 6: # LEFT
-                self.left_right_velocity = 20
-            elif gesture_id == 7: # RIGHT
-                self.left_right_velocity = -20
-            """
-
-            if gesture_id == 0:  # TURN MOTOR ON
-                if (self._is_flying == False):
+            if gesture_id == 0:  # TURN MOTOR ON / FORWARD
+                if (not self._is_flying):
                     self.tello.turn_motor_on()
-            elif gesture_id == 1: # TURN MOTOR OFF
-                if (self._is_flying == False):
+                else:
+                    self.tello.move_forward(20)
+
+            elif gesture_id == 1: # TURN MOTOR OFF / BACKWARD
+                if (not self._is_flying):
                     self.tello.turn_motor_off()
+                else:
+                    self.tello.move_backward(20)
 
             elif gesture_id == 2:  # TAKEOFF
-                if (self._is_flying == False):
+                if (not self._is_flying):
                     self.tello.takeoff()
                     self._is_flying = True
 
             elif gesture_id == 3:  # LAND
                 self._is_landing = True
-                self.forw_back_velocity = self.up_down_velocity = \
-                    self.left_right_velocity = self.yaw_velocity = 0
                 self.tello.land()
                 self._is_flying = False
+
+            elif gesture_id == 4:  # UP
+                if (self._is_flying):
+                    self.tello.move_up(20)
+
+            elif gesture_id == 5:  # DOWN
+                if (self._is_flying):
+                    self.tello.move_down(20)
+
+            elif gesture_id == 6:  # RIGHT
+                if (self._is_flying):
+                    self.tello.move_left(20)
+
+            elif gesture_id == 7:  # LEFT
+                if (self._is_flying):
+                    self.tello.move_right(20)
+
+            elif gesture_id == 8:  # CW
+                if (self._is_flying):
+                    self.tello.rotate_clockwise(360)
+
+            elif gesture_id == 9:  # CCW
+                if (self._is_flying):
+                    self.tello.rotate_counter_clockwise(360)
+
                 
             elif gesture_id == -1:
                 self.forw_back_velocity = self.up_down_velocity = \
                     self.left_right_velocity = self.yaw_velocity = 0
 
-            self.tello.send_rc_control(self.left_right_velocity, self.forw_back_velocity,
-                                       self.up_down_velocity, self.yaw_velocity)
+
